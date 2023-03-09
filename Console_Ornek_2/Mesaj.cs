ec7f84be7b0e977d41a5e685fe86673871d312b2
m.sinans <msomercikoglu@gmail.com>
2023-03-09 17:57:34 +0300

    Add project files.

---
+++ Console_Ornek_2/Mesaj.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ornek_2
+{
+    internal class Mesaj
+    {
+        public void metin()
+        {
+            Console.WriteLine("Hallo mein Freund!");
+            Console.Write("Wie geht es dir?");
+        }
+    }
+}
---
+++ Console_Ornek_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Console_Ornek_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaj msj = new Mesaj();
+            msj.metin();
+            Console.Read();
+
+        }
+    }
+}
